27f173eda82c2323b84a1c1523589aa3f7fbafb4
HjortheimerDaniel <147121561+HjortheimerDaniel@users.noreply.github.com>
2025-03-30 12:55:22 +0900

    nn

--- Struct.h
+++ Struct.h
@@ -4,6 +4,10 @@ struct Vector2 {
 	float x, y;
 };
 
+struct Quad {
+	Vector2 UL, UR, LL, LR;
+};
+
 struct PlayerStruct
 {
 	Vector2 pos, size;
@@ -15,11 +19,17 @@ struct PlayerStruct
 
 struct EnemyStruct
 {
-	Vector2 pos, size;
-	int handle;
-	float speed;
+	Quad pos;
+	Quad hitbox;
+	Vector2 speed, size;
+	int handle, animation, frames, MAXAnimation, MAXFrames;
+	bool isDead, isDamaged;
+	int damagedTimer;
 	float theta;
 	float amplitude;
+	int HP;
+	int color;
+	bool isDead;
 
 };
 
@@ -29,6 +39,15 @@ struct Bullet
 	Vector2 size;
 	bool isShot[10];
 	int speed[10];
+	int handle;
+	int CD;
+};
+
+struct BulletStruct {
+	Vector2 pos[4];
+	int radius;
+	bool isShot[4];
+	int speed[4];
 	int handle;
 	int CD;
 };
